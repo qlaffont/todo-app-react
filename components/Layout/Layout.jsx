cc5be23524e83bfa450765679b90aeda6840d691
Quentin Laffont <qlaffont13@gmail.com>
2019-04-27 19:29:49 +0200

    Add Layout System

---
+++ components/Layout/Layout.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Head from "next/head";
+import Footer from "./Footer";
+import "../../assets/css/main.css";
+
+const Header = props => {
+  return (
+    <div>
+      <Head>
+        <title>My page title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <div>
+        <div>{props.children}</div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+
 export default function Index() {
   return (
-    <div>
-      <p>This is the index page</p>
-    </div>
+    <Layout>
+      <div>
+        <p>This is the index paezeezge</p>
+      </div>
+    </Layout>
   );
 }
