2ddcc705947ed84b85c63ad1da02efc178ac4db3
Quentin <qlaffont@wemanity.com>
2019-09-13 14:02:34 +0200

    Add Docker Integration

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,18 +1,17 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from "react";
-import App, { Container } from "next/app";
+import App from "next/app";
 import { transitions, positions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-oldschool-dark";
 import Head from "next/head";
 
 import { appWithTranslation } from "../i18n";
 
-// optional cofiguration
+// optional cofiguration for alert
 const alertOptions = {
-  // you can also just use 'bottom center'
   position: positions.MIDDLE,
   timeout: 5000,
   offset: "30px",
-  // you can also just use 'scale'
   transition: transitions.SCALE
 };
 
@@ -20,18 +19,21 @@ class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <Container>
+      <div>
         <Head>
           <title>Welcome to totask !</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.ico" />
+          <style>
+            @import url(https://use.fontawesome.com/releases/v5.6.3/css/fontawesome.css)
+          </style>
+          <style>@import url(https://use.fontawesome.com/releases/v5.6.3/css/solid.css)</style>
+          <style>@import url(https://use.fontawesome.com/releases/v5.6.3/css/regular.css)</style>
         </Head>
-        <style>@import url(https://use.fontawesome.com/releases/v5.6.3/css/fontawesome.css)</style>
-        <style>@import url(https://use.fontawesome.com/releases/v5.6.3/css/solid.css)</style>
-        <style>@import url(https://use.fontawesome.com/releases/v5.6.3/css/regular.css)</style>
         <AlertProvider template={AlertTemplate} {...alertOptions}>
           <Component {...pageProps} />
         </AlertProvider>
-      </Container>
+      </div>
     );
   }
 }
